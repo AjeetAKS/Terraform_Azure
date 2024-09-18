5b944411796b2ccd3b5d09dd06cbfdf4f083f324
AjeetAKS <92211558+AjeetAKS@users.noreply.github.com>
2024-09-19 04:30:27 +0530

    Update config.tf
    Adding backend arguments

--- config.tf
+++ config.tf
@@ -1,19 +1,30 @@
 terraform {
+  required_version = ">=1.0"
   required_providers {
     azurerm ={
-        version = ">=2.64.0"
+        version = ">=2.0"
         source = "hashicorp/azurerm"
     }
+    random = {
+      version = "3.0"
+      source = "hashicorp/random"
+    }
   }
-
+  backend "azurerm" {
+    resource_group_name = "terraform-backend"
+    storage_account_name = "backendstorageterraprod"
+    container_name = "tfstatefiles"
+    key = "tfstate.prod"
+    
+  }
 }
 
 provider "azurerm" {
-    client_id = "XXXXXXXXXXXXXXXXXXXXXX"
-    client_secret = "XXXXXXXXXXXXXXXXXXXXXX"
-    tenant_id = "XXXXXXXXXXXXXXXXXXXXXX"
-    subscription_id = "XXXXXXXXXXXXXXXXXXXXXX"
-    features {}
-    
+    client_id = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+    client_secret = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+    tenant_id = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+    subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+  features {}
   
 }
+
